c6316e49045384f9a8695dae7eb5dfe061dbcee1
jasikor <51225921+jasikor@users.noreply.github.com>
2023-04-21 13:53:57 +0200

    Folder.MoveUp implemented, more tests needed.

--- BookModel/Folder.cs
+++ BookModel/Folder.cs
@@ -1,6 +1,6 @@
 ﻿namespace BookModel;
 
-public record Folder(string Title="(folder)") : BinderEntry(Title)
+public record Folder(string Title = "(folder)") : BinderEntry(Title)
 {
     public IEnumerable<BinderEntry> SubFolders = new List<BinderEntry>();
 }
@@ -9,4 +9,14 @@ public static class FolderExt
 {
     public static Folder Add(this Folder @this, BinderEntry entry) =>
         @this with {SubFolders = @this.SubFolders.Append(entry)};
+
+    public static IEnumerable<Folder> MoveUp(this IEnumerable<Folder> @this, int index) =>
+        @this.Swap(index - 1, index);
+
+    public static IEnumerable<Folder> Swap(this IEnumerable<Folder> @this, int index1, int index2)
+    {
+        var l = @this.ToList();
+        (l[index1], l[index2]) = (l[index2], l[index1]);
+        return l;
+    }
 }
--- Tests/FolderTests.cs
+++ Tests/FolderTests.cs
@@ -14,4 +14,26 @@ public class FolderTests
     [Fact]
     public void Add_AddsItem() =>
         new Folder().Add(new Folder()).SubFolders.Count().Should().Be(1);
+
+    [Fact]
+    public void MoveUp_SwapsElements()
+    {
+        // Arrange
+        var l = new[] {
+            new Folder("f1"),
+            new Folder("f2"),
+            new Folder("f3")
+        };
+
+        var exp = new[] {
+            new Folder("f2"),
+            new Folder("f1"),
+            new Folder("f3"),
+        };
+        // Act
+        var actual  = l.MoveUp(1);
+
+        // Assert        
+        actual.Should().BeEquivalentTo(exp, o=>o.IncludingInternalFields().WithStrictOrdering());
+    }
 }
