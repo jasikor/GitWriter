a9294aa535e125f61dfa0ecdd197235feff3c98c
jasikor <51225921+jasikor@users.noreply.github.com>
2023-06-05 09:48:57 +0200

    unused function removed

--- BookModel/Binder/FolderExt.cs
+++ BookModel/Binder/FolderExt.cs
@@ -60,8 +60,4 @@ public static class FolderExt
         return new Folder(folder.Title) {Items = f};
     }
 
-    public static IList<T> Clone<T>(this IList<T> listToClone) where T : ICloneable
-    {
-        return listToClone.Select(item => (T) item.Clone()).ToList();
-    }
 }
